b693fa58aa97be8ac2a946d3023d8d60cd4f31cd
youkwa <42166340+youkwa@users.noreply.github.com>
2023-05-15 16:36:02 +0900

    2023-05-15
    전화번호를 입력하면 번호에 따라 지역을 알려주는 코드입니다. ★를 채워주세요.

---
+++ homework_1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class homework_1 {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("전호번호를 입력하세요.(서울02, 경기031, 인천032, 강원도033) ※-제외");
+        String number = scanner.nextLine();
+        String nu = number.substring(★,★);
+        int ber = Integer.parseInt(nu);
+        String m1 = number.substring(★,★);
+        int m2 = Integer.parseInt(m1);
+
+        if (ber == 2) {
+               System.out.println("서울 입니다.");
+            } else if ( m2 == 31 ){
+            System.out.println("경기도 입니다.");
+        }else if ( m2 == 32 ){
+            System.out.println("인천 입니다.");
+    }else if ( m2 == 33 ) {
+            System.out.println("강원도 입니다.");
+        } else {
+            System.out.println("전화번호를 제대로 눌러주세요.");
+        }
+    }
+}
